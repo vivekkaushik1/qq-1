99fc9f1f64587e7600153bc9b27e082d30c005b5
vivekkaushik1 <vivekit388@gmail.com>
2022-09-02 15:27:42 +0530

    2:randfile_2

--- createCOmmit.sh
+++ createCOmmit.sh
@@ -5,7 +5,9 @@ while [ $x -le 2 ]
  do
    git tag -a PerformanceTest11.$((j+x)) -m "vversion Pt1.1$((j+x))"
    sh ./createCOmmitsTags.sh
+   git pull
    x=$(( $x + 1 ))
+   git merge
    echo "Completed Script Invocaton"
 done
 #git push
